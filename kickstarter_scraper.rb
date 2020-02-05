da879c1769009e275d1275cf4717e395e5a13773
generic-puppet-7755 <tasmith1990@gmail.com>
2020-02-05 00:35:28 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -6,7 +6,7 @@ def create_project_hash
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
   binding.pry
-
+  
 end
 
 # projects: kickstarter.css("li.project.grid_4")
