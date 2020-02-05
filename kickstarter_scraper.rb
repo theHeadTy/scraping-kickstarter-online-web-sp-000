d1a4434eb579409c46c12e2188417327f14fdb1f
generic-puppet-7755 <tasmith1990@gmail.com>
2020-02-05 00:52:55 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -14,5 +14,6 @@ end
 # image link: project.css("div.project-thumbnail a img").attribute("src").value
 # description: project.css("p.bbcard_blurb").text
 # location: project.css("span.location-name").text
+# percent_funded: 
 
 create_project_hash
